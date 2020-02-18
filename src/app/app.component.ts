fd32f36dbfd6fc016a608ffb33be4f185610a1a5
RONAN MC DONAGH - STUDENT <G00373586@gmit.ie>
2020-02-18 17:43:30 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count: number = 0;
+  hideLabel: boolean = true;
+
+  buttonPressed() {
+    console.log("button pressed");
+    this.count++;
+  }
+
+  starPressed() {
+    console.log("star pressed");
+    if (this.hideLabel == true) {
+      this.hideLabel = false;
+    }
+    else {
+      this.hideLabel = true;
+    }
+  }
 }
